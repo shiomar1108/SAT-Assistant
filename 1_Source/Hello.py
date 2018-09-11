45bf3b09f22c868a4d61c83e655d954ad2595d66
Shiomar Salazar <shiomarsa2@gmail.com>
2018-09-10 20:20:22 -0500

    Added progress from today class

--- 1_Source/Hello.py
+++ 1_Source/Hello.py
@@ -1,14 +1,26 @@
 from selenium import webdriver
 
+#Read Credentials
+file = open("/home/shiomar/Desktop/SAT/1_Source/1_Keys/credenciales.txt", "r")
+rfc = file.readline()
+clave = file.readline()
+
 #Initialize Firefox explorer
 browser = webdriver.Firefox()
 # Reach CDFI Portal
 browser.get('https://portalcfdi.facturaelectronica.sat.gob.mx/')
-#Click e.firma option to avoid captcha
-browser.find_element_by_css_selector('.btn.btn-default').click()
 
-#Set private key from file
-element = browser.find_element_by_id('btnPrivateKey').click()
+element = browser.find_element_by_id('rfc')
+element = element.send_keys(rfc)
 
+#Write Password
+element = browser.find_element_by_id('password')
+element = element.send_keys(clave)
 
-#browser.find_element_by_id('btnCertificate').click()
+#Captcha part
+text = raw_input("Write the Captcha")
+element = browser.find_element_by_id('jcaptcha')
+element = element.send_keys(text)
+
+#Submit form
+element = browser.find_element_by_id('submit').click()
