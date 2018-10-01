f0a5d8ce38949ce64455f4b8e747c844e05c67a0
Shiomar Salazar <shiomarsa2@gmail.com>
2018-10-01 18:12:58 -0500

    Added feature for exceptions

--- 1_Source/Hello.py
+++ 1_Source/Hello.py
@@ -4,116 +4,120 @@ from time import sleep
 import requests
 import os, sys
 from PIL import Image
+from selenium.common.exceptions import *
 
-# Function to save the Captcha image
-def get_captcha(driver, element, path):
-    # now that we have the preliminary stuff out of the way time to get that image :D
-    location = element.location
-    size = element.size
-    # saves screenshot of entire page
-    driver.save_screenshot(path)
+try:
+	# Function to save the Captcha image
+	def get_captcha(driver, element, path):
+	    # now that we have the preliminary stuff out of the way time to get that image :D
+	    location = element.location
+	    size = element.size
+	    # saves screenshot of entire page
+	    driver.save_screenshot(path)
 
-    # uses PIL library to open image in memory
-    image = Image.open(path)
+	    # uses PIL library to open image in memory
+	    image = Image.open(path)
 
-    left = location['x']
-    top = location['y']
-    right = location['x'] + size['width']
-    bottom = location['y'] + size['height']
+	    left = location['x']
+	    top = location['y']
+	    right = location['x'] + size['width']
+	    bottom = location['y'] + size['height']
 
-    image = image.crop((left, top, right, bottom))  # defines crop points
-    image.save(path, 'png')  # saves new cropped image
-
-
-# Global variables
-months = ['01','02','03','04','05','06','07','08','09','10','11','12']
-
-# Read Credentials from file and year for calculations.
-file = open("/home/shiomar/Desktop/SAT/1_Source/1_Keys/credenciales.txt", "r")
-rfc = file.readline()
-clave = file.readline()
-year = file.readline()
-file.close()
-
-#Configure Firefox Web Driver to set the absolute path for the XML files.
-downloadPath = os.getcwd() + '/2_XMLS'
-profile = webdriver.FirefoxProfile()
-profile.set_preference("browser.download.folderList", 2)
-profile.set_preference("browser.download.manager.showWhenStarting", False)
-profile.set_preference("browser.download.dir", downloadPath)
-profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/octet-stream")
-
-# Initialize Firefox explorer.
-browser = webdriver.Firefox(firefox_profile=profile)
-
-# Resize the window to the screen width/height
-browser.set_window_size(300, 500)
-
-# Reach CDFI Portal
-browser.get('https://portalcfdi.facturaelectronica.sat.gob.mx/')
-
-# Write RFC.
-element = browser.find_element_by_id('rfc')
-element = element.send_keys(rfc)
-
-# Write Password.
-element = browser.find_element_by_id('password')
-element = element.send_keys(clave)
-
-# Saving Captcha image.
-imgPath = os.getcwd() + '/3_Captcha/captcha.png'
-pic = browser.find_element_by_xpath( "//label[@for='jcaptcha']")
-get_captcha(browser, pic, imgPath)
+	    image = image.crop((left, top, right, bottom))  # defines crop points
+	    image.save(path, 'png')  # saves new cropped image
 
 
+	# Global variables
+	months = ['01','02','03','04','05','06','07','08','09','10','11','12']
 
-# Captcha part.
-text = raw_input("Write the Captcha...  ")
-element = browser.find_element_by_id('jcaptcha')
-element = element.send_keys(text)
+	# Read Credentials from file and year for calculations.
+	file = open("/home/shiomar/Desktop/SAT/1_Source/1_Keys/credenciales.txt", "r")
+	rfc = file.readline()
+	clave = file.readline()
+	year = file.readline()
+	file.close()
 
-# Submit form.
-element = browser.find_element_by_id('submit').click()
+	#Configure Firefox Web Driver to set the absolute path for the XML files.
+	downloadPath = os.getcwd() + '/2_XMLS'
+	profile = webdriver.FirefoxProfile()
+	profile.set_preference("browser.download.folderList", 2)
+	profile.set_preference("browser.download.manager.showWhenStarting", False)
+	profile.set_preference("browser.download.dir", downloadPath)
+	profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/octet-stream")
 
-# Delay for resource optimization.
-sleep(5)
+	# Initialize Firefox explorer.
+	browser = webdriver.Firefox(firefox_profile=profile)
 
-# Click round button and submit.
-element = browser.find_element_by_id('ctl00_MainContent_RdoTipoBusquedaReceptor').click() 
-element = browser.find_element_by_id('ctl00_MainContent_BtnBusqueda').click()
-sleep(5)
+	# Resize the window to the screen width/height
+	browser.set_window_size(1000, 1000)
 
-# Select date conditions.
-element = browser.find_element_by_id('ctl00_MainContent_RdoFechas').click()
+	# Reach CDFI Portal
+	browser.get('https://portalcfdi.facturaelectronica.sat.gob.mx/')
 
-# Set year of calculation.
-s1 = Select(browser.find_element_by_id('DdlAnio'))
-s1.select_by_visible_text(str(year))
+	# Write RFC.
+	element = browser.find_element_by_id('rfc')
+	element = element.send_keys(rfc)
 
-# Set "vigente" as a parameter.
-s1 = Select(browser.find_element_by_id('ctl00_MainContent_DdlEstadoComprobante'))
-s1.select_by_visible_text('Vigente')
+	# Write Password.
+	element = browser.find_element_by_id('password')
+	element = element.send_keys(clave)
 
-for x in months:
-	# Set the Window context in the proper range to avoid errors.
-	browser.execute_script("scrollBy(0,100);")
-	#Delay for resource optimization.
-	sleep(5)
-	# Set month of calculation and submit
-	s1 = Select(browser.find_element_by_name('ctl00$MainContent$CldFecha$DdlMes'))
-	s1.select_by_visible_text(x)
-	element = browser.find_element_by_id('ctl00_MainContent_BtnBusqueda').click()
+	# Saving Captcha image.
+	imgPath = os.getcwd() + '/3_Captcha/captcha.png'
+	pic = browser.find_element_by_xpath( "//label[@for='jcaptcha']")
+	get_captcha(browser, pic, imgPath)
+
+	# Captcha part.
+	text = raw_input("Write the Captcha...  ")
+	element = browser.find_element_by_id('jcaptcha')
+	element = element.send_keys(text)
+
+	# Submit form.
+	element = browser.find_element_by_id('submit').click()
+
 	# Delay for resource optimization.
-	sleep(15)
+	sleep(5)
 
-	# XML Download Cycle, this cycle will downloadd all the xmls of the current month.
-	xml = browser.find_elements_by_class_name("BtnDescarga")
-	for e in xml:
-		e.click()
-		sleep(3)
+	# Click round button and submit.
+	element = browser.find_element_by_id('ctl00_MainContent_RdoTipoBusquedaReceptor').click() 
+	element = browser.find_element_by_id('ctl00_MainContent_BtnBusqueda').click()
+	sleep(5)
 
-	# Message of completation a month.
-	print('Full month done...')
+	# Select date conditions.
+	element = browser.find_element_by_id('ctl00_MainContent_RdoFechas').click()
 
-# Close browser after whole download process.
-browser.close()
+	# Set year of calculation.
+	s1 = Select(browser.find_element_by_id('DdlAnio'))
+	s1.select_by_visible_text(str(year))
+
+	# Set "vigente" as a parameter.
+	s1 = Select(browser.find_element_by_id('ctl00_MainContent_DdlEstadoComprobante'))
+	s1.select_by_visible_text('Vigente')
+
+	for x in months:
+		# Set the Window context in the proper range to avoid errors.
+		browser.execute_script("scrollBy(0,100);")
+		#Delay for resource optimization.
+		sleep(5)
+		# Set month of calculation and submit
+		s1 = Select(browser.find_element_by_name('ctl00$MainContent$CldFecha$DdlMes'))
+		s1.select_by_visible_text(x)
+		element = browser.find_element_by_id('ctl00_MainContent_BtnBusqueda').click()
+		# Delay for resource optimization.
+		sleep(15)
+
+		# XML Download Cycle, this cycle will downloadd all the xmls of the current month.
+		xml = browser.find_elements_by_class_name("BtnDescarga")
+		for e in xml:
+			e.click()
+			sleep(3)
+
+		# Message of completation a month.
+		print('Full month done...')
+
+	# Close browser after whole download process.
+	browser.close()
+except (NoSuchElementException, ElementClickInterceptedException) as e:
+	print('Exception been raised...')
+	browser.execute_script('arguments[0].scrollIntoView(true);', refresh_button)
+	refresh_button.click()
